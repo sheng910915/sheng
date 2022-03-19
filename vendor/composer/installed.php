491436b4d8b3dfee2f6ebbf8e5f95fe50f118c8e
sunguangsheng <sunguangsheng@yuanxinjituan.com>
2022-03-19 11:04:28 +0800

    no message

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6d479bc812a7e5e78c4a3707c5da7980cf95b54f',
+        'name' => 'sophie/sound',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sophie/sound' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6d479bc812a7e5e78c4a3707c5da7980cf95b54f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
